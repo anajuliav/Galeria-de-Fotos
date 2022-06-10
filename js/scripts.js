4e7f8abc82b914b80d5c2647ba4670d00e05bd36
anajuliav <av7121217@gmail.com>
2022-06-10 00:39:11 -0300

    iniciado o projeto de Galeria

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-$(document).ready(function() {
-    $('.image-link').magnificPopup({type:'image'});
-  });
+$('.Galeria002').magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'image',
+   gallery:{enabled:true}
+  // other options
+});
